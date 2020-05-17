bdd22ddf819ad3b931cd50628bee0478acb73cc5
hongjianguo <778464081@qq.com>
2020-05-17 13:35:22 +0800

    0517 六十甲子

--- 0516homework/jiazi.java
+++ 0516homework/jiazi.java
@@ -1,19 +1,19 @@
 
     public class jiazi {
-    public static void main(String[] args) {
-    int count=0;
-    String arr[]={"甲","乙","丙","丁","戊","己","庚","辛","壬","癸"};
-    String arr1[]={"子","丑","寅","卯","辰来","巳","午","未","申源","酉","戌","亥"};
-     for(int i=0;i<arr.length;i++){
-             for(int j=0;j<arr1.length;j++){
-              count++;
-             if(count%10==0){
-          System.out.println(arr[i]+arr1[j]);
-    }
-    else{
-    System.out.print(arr[i]+arr1[j]+", ");
-    }
-    }
-    }
-    }
+        public static void main( String[] args ) {
+  
+            char[] heavenlyStems = { '甲' , '乙' , '丙' , '丁' , '戊' , '己' , '庚' , '辛' , '壬' , '癸' };
+            
+            char[] earthlyBranches = { '子' , '丑' , '寅' , '卯' , '辰' , '巳' ,'午' , '未' , '申' , '酉' , '戌' , '亥' };
+           int x;
+           int y;
+            for(int i=0;i<60;i++){
+                x=i%10;
+                y=i%12;
+                System.out.print(heavenlyStems[x] + "" + earthlyBranches[y] + " ");
+                if((i+1)%12==0 ){
+                    System.out.println();
+                }
+            }
+          }
     }
