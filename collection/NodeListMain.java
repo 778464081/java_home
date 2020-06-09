1c8d906242e72420dbfaefd4411c930748cdbc5c
洪建国 <778464081@qq.com>
2020-06-09 23:11:46 +0800

    笔记更新

---
+++ collection/NodeListMain.java
@@ -0,0 +1,14 @@
+package T1;
+
+import java.util.LinkedList;
+
+public class NodeListMain {
+    public static void main(String[] args) {
+        NodeList one =new NodeList();
+        one.add("casd");
+        one.add("cd");
+        one.add("d");
+        System.out.println(one);
+        LinkedList
+    }
+}
